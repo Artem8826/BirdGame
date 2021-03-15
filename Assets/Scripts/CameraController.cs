a5ec8dcb7c2b32cf38918cdea46aa8924760e44a
Kendshi <anton_gamayunov@mail.ru>
2021-03-16 00:58:44 +0300

    Добавил локацию дерево

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector2 startPos;
+    private Camera cam;
+    private Transform selfTransform;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+        selfTransform = transform;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = cam.ScreenToWorldPoint(Input.mousePosition);
+            
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            float posX = cam.ScreenToWorldPoint(Input.mousePosition).x - startPos.x;
+            float posY = cam.ScreenToWorldPoint(Input.mousePosition).y - startPos.y;
+            selfTransform.position = 
+                new Vector3(Mathf.Clamp(selfTransform.position.x - posX, -0.455f, 0.455f), Mathf.Clamp(selfTransform.position.y - posY, -0.702f, 0.702f), selfTransform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/ResursesSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResursesSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] spawnPoints;
+
+    private void Update()
+    {
+        
+    }
+
+
+    private void Createobject()
+    { 
+        
+    }
+}
